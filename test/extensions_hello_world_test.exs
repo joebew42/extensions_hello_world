f82c36c92bb59ee96697e8ce515332855fe7e6f8
JoeBew42 <joebew42@gmail.com>
2019-06-01 09:59:43 +0200

    remove uneeded files

--- test/extensions_hello_world_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExtensionsHelloWorldTest do
-  use ExUnit.Case
-  doctest ExtensionsHelloWorld
-
-  test "greets the world" do
-    assert ExtensionsHelloWorld.hello() == :world
-  end
-end
