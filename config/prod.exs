2f350a6b00070d591bd8e18be7ba7f9c2964cad5
JoeBew42 <joebew42@gmail.com>
2019-06-01 17:23:06 +0200

    use JWT_TOKEN_AUTHENTICATOR_SECRET env var

---
+++ config/prod.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+config :extensions_hello_world,
+  token_authenticator: ExtensionsHelloWorld.JWTTokenAuthenticator,
+  change_color_use_case: ExtensionsHelloWorld.ChangeColorUseCase
+
+config :extensions_hello_world,
+  jwt_token_authenticator_secret: "" # Use the JWT_TOKEN_AUTHENTICATOR_SECRET env variable
