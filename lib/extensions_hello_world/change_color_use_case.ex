74fff84152cc82572eb0fab51fa7aee4eccd13b8
JoeBew42 <joebew42@gmail.com>
2019-06-01 17:16:25 +0200

    fake a user

--- lib/extensions_hello_world/change_color_use_case.ex
+++ lib/extensions_hello_world/change_color_use_case.ex
@@ -2,7 +2,12 @@ defmodule ExtensionsHelloWorld.ChangeColorUseCase do
   @behaviour ExtensionsHelloWorld.UseCase
 
   @impl true
-  def run_with(channel_id: _channel_id, user_id: _user_id) do
-    {:error, "user is in cool down"}
+  def run_with(channel_id: _channel_id, user_id: user_id) do
+    case user_id do
+      "A COOL DOWN USER ID" ->
+        {:error, "user is in cool down"}
+      "A USER ID" ->
+        {:ok, "user is changing color"}
+    end
   end
 end
--- test/extensions_hello_world/change_color_use_case_test.exs
+++ test/extensions_hello_world/change_color_use_case_test.exs
@@ -4,10 +4,18 @@ defmodule ExtensionsHelloWorld.ChangeColorUseCaseTest do
   alias ExtensionsHelloWorld.ChangeColorUseCase, as: ChangeColor
 
   describe "when the user is in cool down" do
-    test "return an error" do
-      response = ChangeColor.run_with(channel_id: "A CHANNEL ID", user_id: "A USER ID")
+    test "it will return an error" do
+      response = ChangeColor.run_with(channel_id: "A CHANNEL ID", user_id: "A COOL DOWN USER ID")
 
       assert response == {:error, "user is in cool down"}
+    end
+  end
+
+  describe "when the user is not in cool down" do
+    test "it will change the color, set a new cool down and notify the change" do
+      response = ChangeColor.run_with(channel_id: "A CHANNEL ID", user_id: "A USER ID")
+
+      assert response == {:ok, "user is changing color"}
     end
   end
 end
