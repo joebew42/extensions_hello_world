e70202d03f4f01dff867e92249be3544081b1377
JoeBew42 <joebew42@gmail.com>
2019-06-02 15:22:26 +0200

    save a user

--- lib/extensions_hello_world/infrastructure/genserver_users.ex
+++ lib/extensions_hello_world/infrastructure/genserver_users.ex
@@ -1,13 +1,33 @@
 defmodule ExtensionsHelloWorld.Infrastructure.GenServerUsers do
   @behaviour ExtensionsHelloWorld.Model.Users
 
+  alias ExtensionsHelloWorld.Model.User
+
+  use GenServer
+
   @impl true
   def find(_user_id) do
     {:error, :not_found}
   end
 
   @impl true
-  def save(_user) do
-    raise "Not Yet Implemented"
+  def save(%User{} = user) do
+    :ok = GenServer.call(__MODULE__, {:save, user})
+  end
+
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+  end
+
+  @impl true
+  def init(state) do
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_call({:save, %User{} = user}, _from, state) do
+    Map.put(state, user.id, user)
+
+    {:reply, :ok, state}
   end
 end
--- test/extensions_hello_world/infrastructure/gen_server_users_test.exs
+++ test/extensions_hello_world/infrastructure/gen_server_users_test.exs
@@ -1,11 +1,28 @@
 defmodule ExtensionsHelloWorld.Infrastructure.GenServerUsersTest do
   use ExUnit.Case, async: true
 
+  alias ExtensionsHelloWorld.Model.User
   alias ExtensionsHelloWorld.Infrastructure.GenServerUsers, as: Users
+
+  setup do
+    start_supervised(Users)
+    :ok
+  end
 
   describe "find" do
     test "return not found when the user not exist" do
       assert Users.find("A USER ID") == {:error, :not_found}
+    end
+  end
+
+  describe "save" do
+    test "return ok when a user is saved" do
+      user = %User{
+        id: "AN ID",
+        cooldown: nil
+      }
+
+      assert Users.save(user) == :ok
     end
   end
 end
